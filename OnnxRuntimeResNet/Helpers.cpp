acd1c59c31f8c439648a2010146c0bad70b49b87
yf711 <yifanl@microsoft.com>
2023-06-12 17:13:36 -0700

    adapt to trt ep

--- OnnxRuntimeResNet/Helpers.cpp
+++ OnnxRuntimeResNet/Helpers.cpp
@@ -11,51 +11,77 @@
 #include<opencv2/highgui.hpp>
 #include<opencv2/imgproc.hpp>
 
+#include <tensorrt_provider_factory.h>
+#include <tensorrt_provider_options.h>
+
+//static std::unique_ptr<OrtTensorRTProviderOptionsV2> get_default_trt_provider_options() {
+//   auto tensorrt_options = std::make_unique<OrtTensorRTProviderOptionsV2>();
+//   tensorrt_options->device_id = 0;
+//   tensorrt_options->has_user_compute_stream = 0;
+//   tensorrt_options->user_compute_stream = nullptr;
+//   tensorrt_options->trt_max_partition_iterations = 1000;
+//   tensorrt_options->trt_min_subgraph_size = 1;
+//   tensorrt_options->trt_max_workspace_size = 1 << 30;
+//   tensorrt_options->trt_fp16_enable = false;
+//   tensorrt_options->trt_int8_enable = false;
+//   tensorrt_options->trt_int8_calibration_table_name = "";
+//   tensorrt_options->trt_int8_use_native_calibration_table = false;
+//   tensorrt_options->trt_dla_enable = false;
+//   tensorrt_options->trt_dla_core = 0;
+//   tensorrt_options->trt_dump_subgraphs = false;
+//   tensorrt_options->trt_engine_cache_enable = false;
+//   tensorrt_options->trt_engine_cache_path = "";
+//   tensorrt_options->trt_engine_decryption_enable = false;
+//   tensorrt_options->trt_engine_decryption_lib_path = "";
+//   tensorrt_options->trt_force_sequential_engine_build = false;
+//
+//   return tensorrt_options;
+//}
 
 static std::vector<float> loadImage(const std::string& filename, int sizeX = 224, int sizeY = 224)
 {
-    cv::Mat image = cv::imread(filename);
-    if (image.empty()) {
-        std::cout << "No image found.";
-    }
+   cv::Mat image = cv::imread(filename);
+   if (image.empty()) {
+      std::cout << "No image found.";
+   }
 
-    // convert from BGR to RGB
-    cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
+   // convert from BGR to RGB
+   cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
 
-    // resize
-    cv::resize(image, image, cv::Size(sizeX, sizeY));
+   // resize
+   cv::resize(image, image, cv::Size(sizeX, sizeY));
 
-    // reshape to 1D
-    image = image.reshape(1, 1);
+   // reshape to 1D
+   image = image.reshape(1, 1);
 
-    // uint_8, [0, 255] -> float, [0, 1]
-    // Normalize number to between 0 and 1
-    // Convert to vector<float> from cv::Mat.
-    std::vector<float> vec;
-    image.convertTo(vec, CV_32FC1, 1. / 255);
+   // uint_8, [0, 255] -> float, [0, 1]
+   // Normalize number to between 0 and 1
+   // Convert to vector<float> from cv::Mat.
+   std::vector<float> vec;
+   image.convertTo(vec, CV_32FC1, 1. / 255);
 
-    // Transpose (Height, Width, Channel)(224,224,3) to (Chanel, Height, Width)(3,224,224)
-    std::vector<float> output;
-    for (size_t ch = 0; ch < 3; ++ch) {
-        for (size_t i = ch; i < vec.size(); i += 3) {
-            output.emplace_back(vec[i]);
-        }
-    }
-    return output;
+   // Transpose (Height, Width, Channel)(224,224,3) to (Chanel, Height, Width)(3,224,224)
+   std::vector<float> output;
+   for (size_t ch = 0; ch < 3; ++ch) {
+      for (size_t i = ch; i < vec.size(); i += 3) {
+         output.emplace_back(vec[i]);
+      }
+   }
+   return output;
 }
 
 static std::vector<std::string> loadLabels(const std::string& filename)
 {
-    std::vector<std::string> output;
+   std::vector<std::string> output;
 
-    std::ifstream file(filename);
-    if (file) {
-        std::string s;
-        while (getline(file, s)) {
-            output.emplace_back(s);
-        }
-        file.close();
-    }
+   std::ifstream file(filename);
+   if (file) {
+      std::string s;
+      while (getline(file, s)) {
+         output.emplace_back(s);
+      }
+      file.close();
+   }
 
-    return output;
+   return output;
 }
--- OnnxRuntimeResNet/OnnxRuntimeResNet.cpp
+++ OnnxRuntimeResNet/OnnxRuntimeResNet.cpp
@@ -2,112 +2,142 @@
 
 #include <onnxruntime_cxx_api.h>
 #include <iostream>
-
+#include <ctime>
 #include "Helpers.cpp"
 
 int main()
 {
-	Ort::Env env;
-	Ort::RunOptions runOptions;
-	Ort::Session session(nullptr);
+   Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "test");
+   const auto& api = Ort::GetApi();
+   Ort::RunOptions runOptions;
+   Ort::Session session(nullptr);
 
-    constexpr int64_t numChannels = 3;
-    constexpr int64_t width = 224;
-    constexpr int64_t height = 224;
-    constexpr int64_t numClasses = 1000;
-    constexpr int64_t numInputElements = numChannels * height * width;
-
-
-    const std::string imageFile = "C:\\code\\cpp-onnxruntime-resnet-console-app\\OnnxRuntimeResNet\\assets\\dog.png";
-    const std::string labelFile = "C:\\code\\cpp-onnxruntime-resnet-console-app\\OnnxRuntimeResNet\\assets\\imagenet_classes.txt";
-    auto modelPath = L"C:\\code\\cpp-onnxruntime-resnet-console-app\\OnnxRuntimeResNet\\assets\\resnet50v2.onnx";
+   constexpr int64_t numChannels = 3;
+   constexpr int64_t width = 224;
+   constexpr int64_t height = 224;
+   constexpr int64_t numClasses = 1000;
+   constexpr int64_t numInputElements = numChannels * height * width;
 
 
-    //load labels
-    std::vector<std::string> labels = loadLabels(labelFile);
-    if (labels.empty()) {
-        std::cout << "Failed to load labels: " << labelFile << std::endl;
-        return 1;
-    }
-
-    // load image
-    const std::vector<float> imageVec = loadImage(imageFile);
-    if (imageVec.empty()) {
-        std::cout << "Failed to load image: " << imageFile << std::endl;
-        return 1;
-    }
-
-    if (imageVec.size() != numInputElements) {
-
-        std::cout << "Invalid image format. Must be 224x224 RGB image." << std::endl;
-        return 1;
-    }
-
-    // Use CUDA GPU
-    Ort::SessionOptions ort_session_options;
-
-    OrtCUDAProviderOptions options;
-    options.device_id = 0;
-    //options.arena_extend_strategy = 0;
-    //options.gpu_mem_limit = 2 * 1024 * 1024 * 1024;
-    //options.cudnn_conv_algo_search = OrtCudnnConvAlgoSearchExhaustive;
-    //options.do_copy_in_default_stream = 1;
-    
-    OrtSessionOptionsAppendExecutionProvider_CUDA(ort_session_options, options.device_id);
-
-    // create session
-    session = Ort::Session(env, modelPath, ort_session_options);
-
-    // Use CPU
-    //session = Ort::Session(env, modelPath, Ort::SessionOptions{ nullptr });
-
-    // define shape
-    const std::array<int64_t, 4> inputShape = { 1, numChannels, height, width };
-    const std::array<int64_t, 2> outputShape = { 1, numClasses };
-
-    // define array
-    std::array<float, numInputElements> input;
-    std::array<float, numClasses> results;
-
-    // define Tensor
-    auto memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
-    auto inputTensor = Ort::Value::CreateTensor<float>(memory_info, input.data(), input.size(), inputShape.data(), inputShape.size());
-    auto outputTensor = Ort::Value::CreateTensor<float>(memory_info, results.data(), results.size(), outputShape.data(), outputShape.size());
-
-    // copy image data to input array
-    std::copy(imageVec.begin(), imageVec.end(), input.begin());
+   const std::string imageFile = "C:\\code\\cpp-onnxruntime-resnet-console-app\\OnnxRuntimeResNet\\assets\\dog.png";
+   const std::string labelFile = "C:\\code\\cpp-onnxruntime-resnet-console-app\\OnnxRuntimeResNet\\assets\\imagenet_classes.txt";
+   auto modelPath = L"C:\\code\\cpp-onnxruntime-resnet-console-app\\OnnxRuntimeResNet\\assets\\resnet50v2.onnx";
 
 
+   //load labels
+   std::vector<std::string> labels = loadLabels(labelFile);
+   if (labels.empty()) {
+      std::cout << "Failed to load labels: " << labelFile << std::endl;
+      return 1;
+   }
 
-    // define names
-    Ort::AllocatorWithDefaultOptions ort_alloc;
-    Ort::AllocatedStringPtr inputName = session.GetInputNameAllocated(0, ort_alloc);
-    Ort::AllocatedStringPtr outputName = session.GetOutputNameAllocated(0, ort_alloc);
-    const std::array<const char*, 1> inputNames = { inputName.get()};
-    const std::array<const char*, 1> outputNames = { outputName.get()};
-    inputName.release();
-    outputName.release();
+   // load image
+   const std::vector<float> imageVec = loadImage(imageFile);
+   if (imageVec.empty()) {
+      std::cout << "Failed to load image: " << imageFile << std::endl;
+      return 1;
+   }
+
+   if (imageVec.size() != numInputElements) {
+      std::cout << "Invalid image format. Must be 224x224 RGB image." << std::endl;
+      return 1;
+   }
+
+   //// One way adding trt options
+   //Ort::SessionOptions sf;
+   //int device_id = 0;
+   //OrtCUDAProviderOptions cuda_options;
+   //cuda_options.device_id = device_id;
+   //cuda_options.do_copy_in_default_stream = true;
+   //Ort::ThrowOnError(OrtSessionOptionsAppendExecutionProvider_Tensorrt(sf, device_id));
+   //sf.AppendExecutionProvider_CUDA(cuda_options);
+
+   // 2nd way adding trt options
+   //**************************************************************************************************************************
+   // It's suggested to use CreateTensorRTProviderOptions() to get provider options
+   // since ORT takes care of valid options for you
+   //**************************************************************************************************************************
+   Ort::SessionOptions session_options;
+   OrtTensorRTProviderOptionsV2* tensorrt_options;
+   Ort::ThrowOnError(api.CreateTensorRTProviderOptions(&tensorrt_options));
+   tensorrt_options->device_id = 0;
+   tensorrt_options->trt_fp16_enable = true;
+   tensorrt_options->trt_dla_enable = true;
+   tensorrt_options->trt_dla_core = 0;
+   tensorrt_options->trt_engine_cache_enable = true;
+   tensorrt_options->trt_engine_cache_path = "C:\\tmp";
+   std::unique_ptr<OrtTensorRTProviderOptionsV2, decltype(api.ReleaseTensorRTProviderOptions)> rel_trt_options(tensorrt_options, api.ReleaseTensorRTProviderOptions);
+   Ort::ThrowOnError(api.SessionOptionsAppendExecutionProvider_TensorRT_V2(static_cast<OrtSessionOptions*>(session_options),
+      rel_trt_options.get()));
+
+   // 3rd way adding trt options
+   //*****************************************************************************************
+   // It's not suggested to directly new OrtTensorRTProviderOptionsV2 to get provider options
+   //*****************************************************************************************
+   //
+   // auto tensorrt_options = get_default_trt_provider_options();
+   // session_options.AppendExecutionProvider_TensorRT_V2(*tensorrt_options.get());
+
+   time_t start, s_created, pre_infer, finish;
+   // stores time in current_time
+   time(&start);
+   std::cout << "Running ORT TRT EP with default provider options" << std::endl;
+
+   // create session
+   session = Ort::Session(env, modelPath, session_options);
+   time(&s_created);
+
+   // define shape
+   const std::array<int64_t, 4> inputShape = { 1, numChannels, height, width };
+   const std::array<int64_t, 2> outputShape = { 1, numClasses };
+
+   // define array
+   std::array<float, numInputElements> input;
+   std::array<float, numClasses> results;
+
+   // define Tensor
+   auto memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
+   auto inputTensor = Ort::Value::CreateTensor<float>(memory_info, input.data(), input.size(), inputShape.data(), inputShape.size());
+   auto outputTensor = Ort::Value::CreateTensor<float>(memory_info, results.data(), results.size(), outputShape.data(), outputShape.size());
+
+   // copy image data to input array
+   std::copy(imageVec.begin(), imageVec.end(), input.begin());
 
 
-    // run inference
-    try {
-        session.Run(runOptions, inputNames.data(), &inputTensor, 1, outputNames.data(), &outputTensor, 1);
-    }
-    catch (Ort::Exception& e) {
-        std::cout << e.what() << std::endl;
-        return 1;
-    }
 
-     // sort results
-    std::vector<std::pair<size_t, float>> indexValuePairs;
-    for (size_t i = 0; i < results.size(); ++i) {
-        indexValuePairs.emplace_back(i, results[i]);
-    }
-    std::sort(indexValuePairs.begin(), indexValuePairs.end(), [](const auto& lhs, const auto& rhs) { return lhs.second > rhs.second; });
+   // define names
+   Ort::AllocatorWithDefaultOptions ort_alloc;
+   Ort::AllocatedStringPtr inputName = session.GetInputNameAllocated(0, ort_alloc);
+   Ort::AllocatedStringPtr outputName = session.GetOutputNameAllocated(0, ort_alloc);
+   const std::array<const char*, 1> inputNames = { inputName.get() };
+   const std::array<const char*, 1> outputNames = { outputName.get() };
+   inputName.release();
+   outputName.release();
 
-    // show Top5
-    for (size_t i = 0; i < 5; ++i) {
-        const auto& result = indexValuePairs[i];
-        std::cout << i + 1 << ": " << labels[result.first] << " " << result.second << std::endl;
-    }
+
+   // run inference
+   try {
+      time(&pre_infer);
+      session.Run(runOptions, inputNames.data(), &inputTensor, 1, outputNames.data(), &outputTensor, 1);
+      time(&finish);
+   }
+   catch (Ort::Exception& e) {
+      std::cout << e.what() << std::endl;
+      return 1;
+   }
+
+   // sort results
+   std::vector<std::pair<size_t, float>> indexValuePairs;
+   for (size_t i = 0; i < results.size(); ++i) {
+      indexValuePairs.emplace_back(i, results[i]);
+   }
+   std::sort(indexValuePairs.begin(), indexValuePairs.end(), [](const auto& lhs, const auto& rhs) { return lhs.second > rhs.second; });
+
+   // show Top5
+   for (size_t i = 0; i < 5; ++i) {
+      const auto& result = indexValuePairs[i];
+      std::cout << i + 1 << ": " << labels[result.first] << " " << result.second << std::endl;
+   }
+   std::cout << "Session creation latency = " << difftime(s_created, start) << " seconds" << std::endl;
+   std::cout << "Inference latency = " << difftime(finish, pre_infer) << " seconds" << std::endl;
 }
